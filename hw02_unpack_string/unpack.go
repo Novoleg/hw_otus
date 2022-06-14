fc4362a8a806ba3428df21018534d9e39e28c21e
novoleg <novoleg2697@yandex.ru>
2022-06-15 00:11:32 +0700

    hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputString string) (string, error) {
+
+	var sb strings.Builder
+	regexpString := `^[0-9]|[0-9][0-9]+`
+	matched, _ := regexp.MatchString(regexpString, inputString)
+	stringSlice := strings.Split(inputString, "")
+
+	// Проверка с помощью регулярки на корректность строки, если строка некорректна, то возвращает ошибку
+	if matched {
+		return "", ErrInvalidString
+	}
+	
+	for i, v := range stringSlice {
+		tmpRune := []rune(v)
+
+		if unicode.IsDigit(tmpRune[0]) {
+			intSubString, _ := strconv.Atoi(v)
+
+			if intSubString == 0 {
+				tmpString := sb.String()
+				sb.Reset()
+				sb.WriteString(tmpString[:len(tmpString)-1])
+
+			} else {
+				newSubString := strings.Repeat((string(stringSlice[i-1])), intSubString-1)
+				sb.WriteString(newSubString)
+			}
+		} else {
+			sb.WriteString(v)
+		}
+	}
+	fmt.Println("Распакованная строка: ", sb.String())
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -2,6 +2,7 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,6 +17,13 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "aaa0bbb0", expected: "aabb"},
+		{input: ",4,.m", expected: ",,,,,.m"},
+		{input: "日本語4", expected: "日本語語語語"},
+		{input: "日本4語", expected: "日本本本本語"},
+		{input: "\u65e54\u8a9e", expected: "\u65e5\u65e5\u65e5\u65e5\u8a9e"},
+		{input: "\U000065e5\U00008a9e3", expected: "\U000065e5\U00008a9e\U00008a9e\U00008a9e"},
+		// {input: "d\\n5abc", expected: "d\\n\\n\\n\\n\\nabc"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -39,6 +47,7 @@ func TestUnpackInvalidString(t *testing.T) {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
+			fmt.Print()
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
